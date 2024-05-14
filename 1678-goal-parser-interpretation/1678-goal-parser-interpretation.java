8fb32bdadd43db571c472998b9a229081a0a47fb
Nikson A <89377596+Nikson-Daniel@users.noreply.github.com>
2024-05-14 21:49:09 +0530

    Time: 2 ms (14.65%), Space: 41.4 MB (54.22%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String interpret(String command) {
+        
+         String result = "";
+
+        for (int i = 0; i < command.length(); i++){
+            //for G
+            if(command.charAt(i)=='G'){
+               result+='G';
+            } else if (command.charAt(i)=='(' && command.charAt(i+1)==')') {
+                result+='o';
+            } else if (command.charAt(i)=='(' && command.charAt(i+1)=='a') {
+                result+="al";
+            }
+
+        }
+
+
+
+
+        return result;
+        
+    }
+}
